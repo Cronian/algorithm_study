8a0f7cdbae8fc94b5f89da0bd78ca2e8d8c8ffb3
dnao9437 <60914749+dnao9437@users.noreply.github.com>
2020-06-21 22:54:54 +0900

    Update 1. decalcomanieSquared.java

--- hj/week17/ex/1. decalcomanieSquared.java
+++ hj/week17/ex/1. decalcomanieSquared.java
@@ -1,28 +1,42 @@
 package test2;
 
-public class symmetryProcession {
+public class decalcomanieSquared {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		int num_matrix = 4;
+		int solution = 0;
 		
-		char[] initMatrix = {'A', 'B', 'C', 'D'};
 		
-		for(int i = 0; i < num_matrix; i++) {
+		for(int tc = 0; tc < 1; tc++) {
 			
-			for(int j = 0; j < num_matrix; j++) {
-				char c = initMatrix[i ^ j];
-				if(j == num_matrix - 1) {
-					System.out.println(c);
-				} else {
-					System.out.print(c + "   ");	
-				}
+			for(int i = 1; i < 301; i++) {
+				solution += makeReverseNumber(i * i, 10);
 			}
 			
 		}
 		
+		System.out.println("총 개수 : " + solution);
 
+	}
+	
+	public static int makeReverseNumber(int num_origin, int b) {
+		
+		int tmp, num_reverse;
+		tmp = num_origin;
+		num_reverse = 0;
+		
+		while(tmp > 0) {
+			num_reverse = (num_reverse * b + (tmp % b));
+			tmp = tmp / b;
+			
+		}
+		
+		if(num_reverse == num_origin) {
+			System.out.println("반전 수가 같음 : " + num_reverse);
+			return 1;
+		} else {
+			return 0;
+		}
 	}
 
 }
