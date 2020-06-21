855fa07a949547ff9025024cfae5fe5e2b1867fd
dnao9437 <60914749+dnao9437@users.noreply.github.com>
2020-06-21 22:55:13 +0900

    Update 2. continuePrimeNumber.java

--- hj/week17/ex/2. continuePrimeNumber.java
+++ hj/week17/ex/2. continuePrimeNumber.java
@@ -1,28 +1,62 @@
 package test2;
 
-public class symmetryProcession {
-
+public class continuePrimeNumber {
+	
+	public static int[][] prime = new int[8][10000];
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		int num_matrix = 4;
-		
-		char[] initMatrix = {'A', 'B', 'C', 'D'};
-		
-		for(int i = 0; i < num_matrix; i++) {
+		for( int tc = 0; tc < 1; ++tc) {
+			extendPrime(4);
 			
-			for(int j = 0; j < num_matrix; j++) {
-				char c = initMatrix[i ^ j];
-				if(j == num_matrix - 1) {
-					System.out.println(c);
-				} else {
-					System.out.print(c + "   ");	
-				}
+			for(int i = 0; prime[4-1][i] > 0; i++) {
+				System.out.println("PrimeNumber : " + prime[4-1][i]);
 			}
-			
 		}
-		
 
 	}
-
+	
+	public static boolean checkPrime(int num) {
+		
+		for(int i = 2; i * i <= num; i++) {
+			if(num % i ==0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static int extendPrime(int n) {
+		int i, j;
+		int num;
+		
+		int k, x;
+		
+		for(i = 0; i < n; i++) {
+			if(i == 0) {
+				if(prime[0][0] == 0) {
+					prime[0][0] = 2;
+					prime[0][1] = 3;
+					prime[0][2] = 5;
+					prime[0][3] = 7;
+				}
+			} else {
+				x = 0;
+				for(j = 0; prime[i - 1][j] > 0; j++) {
+					for(k = 0; k < 5; k++) {
+						num = prime[i-1][j] * 10 + 2 * k + 1;
+						System.out.println("candidated num : " + num);
+						
+						if((prime[i][x] == 0) && (checkPrime(num))) {
+							prime[i][x] = num;
+							x++;
+						}
+					}
+				}
+			}
+		}
+		
+		return 0;
+	}
+	
 }
