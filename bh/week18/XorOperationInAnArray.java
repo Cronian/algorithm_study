cbe2f7d672ff08fca87306a3e4fa42451bb99ef3
byunghwi <gpdldy91@gmail.com>
2020-06-28 23:54:10 +0900

    Create XorOperationInAnArray.java

---
+++ bh/week18/XorOperationInAnArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        
+        int XOR = 0;
+        
+        for(int i=0; i<n; i++)
+        {
+            XOR^=(start+2*i);
+        }
+        
+        return XOR;
+    }
+}
