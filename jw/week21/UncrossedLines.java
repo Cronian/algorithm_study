39a65258e407f5fa8a9d25a6ef874dfa1cf08844
Cronian <Cronian89@gmail.com>
2020-07-20 09:40:18 +0900

    Update UncrossedLines.java

--- jw/week21/UncrossedLines.java
+++ jw/week21/UncrossedLines.java
@@ -15,3 +15,41 @@ class Solution {
         return dp[lenA][lenB];
     }
 }
+/*
+class Solution {
+    private int sizeA;
+    private int sizeB;
+    private int maxCount;
+    public int maxUncrossedLines(int[] A, int[] B) {
+        this.sizeA = A.length;
+        this.sizeB = B.length;
+        this.maxCount = 0;
+        calc(A,B,0,-1,0,new ArrayList<Integer>());
+        return maxCount;
+    }
+    
+    private void calc(int[] A,int[] B,int idx,int minB,int count,List<Integer> out) {
+        
+        if(idx >= this.sizeA || minB+1 >= this.sizeB ) {
+            if(maxCount < count) this.maxCount = count;
+        }
+        else {
+            int sIdx = -1;
+            if(!out.contains(A[idx])) {
+                for(int i=minB+1;i<this.sizeB;i++) {
+                    if(A[idx] == B[i]) {
+                        sIdx = i;
+                        break;
+                    } 
+                }
+                if(sIdx != -1) {   
+                    List<Integer> nout = new ArrayList<>(out);
+                    calc(A,B,idx+1,sIdx,count+1,nout);
+                }
+                else out.add(A[idx]);
+            }
+            calc(A,B,idx+1,minB,count,out);
+        }
+    }
+}
+*/
