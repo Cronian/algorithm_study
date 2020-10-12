6d631901239a1ff05bf5afe90a1e2442c4846229
Cronian <Cronian89@gmail.com>
2020-10-12 22:05:50 +0900

    Update firstMissingPositive.java

--- jw/week32/firstMissingPositive.java
+++ jw/week32/firstMissingPositive.java
@@ -2,7 +2,7 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         Set<Integer> hs = new HashSet<>();
         for(int t : nums ) hs.add(t);
-        return IntStream.rangeClosed(1,nums.length+1).filter(p->{
+        return IntStream.rangeClosed(1,nums.length).filter(p->{
             if(hs.contains(p)) return false;
             else return true;
         }).limit(1).max().orElse(nums.length+1);
