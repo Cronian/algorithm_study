04440c34dfe10cdc878514381342ebfc73f7e7dd
sunjae-Jeong <57482389+nJae94@users.noreply.github.com>
2021-03-07 21:47:33 +0900

    Create  Maximum Subarray.js

---
+++ sj/50week/Maximum Subarray.js
@@ -0,0 +1,11 @@
+//베스트 솔류션
+function maxSubArray(A) {
+  var prev = 0;
+  var max = -Number.MAX_VALUE;
+
+  for (var i = 0; i < A.length; i++) {
+    prev = Math.max(prev + A[i], A[i]);
+    max = Math.max(max, prev);
+  }
+  return max;
+}
