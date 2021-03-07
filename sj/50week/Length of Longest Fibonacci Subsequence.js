8f1378610d6a5faa6a4085e43046bab831bba974
sunjae-Jeong <57482389+nJae94@users.noreply.github.com>
2021-03-07 21:49:28 +0900

    Update Length of Longest Fibonacci Subsequence.js

--- sj/50week/Length of Longest Fibonacci Subsequence.js
+++ sj/50week/Length of Longest Fibonacci Subsequence.js
@@ -22,3 +22,52 @@ var lenLongestFibSubseq = function(A) {
 
     return ans >= 3 ? ans : 0;
 };
+
+
+//내가 푼거
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var lenLongestFibSubseq = function(arr) {
+    
+    let map = new Map();
+    
+    let len = 0;
+    
+    for(let a of arr)
+        {
+            map.set(a,true);
+        }
+    
+    arr.sort((a,b)=> a-b);
+    
+    
+    for(let i=0; i<arr.length; i++)
+        {
+            for(let j=i+1; j<arr.length; j++)
+                {
+                    let right = arr[j];
+                    
+                    let left = arr[i];
+                    
+                    let templen = 0;
+                    
+                    //두개 더한게 있는지
+                    while(map.has(left+right))
+                        {
+                            templen++;
+                            // 앞에 두개 더한값
+                            let temp = left+right;
+                            // 앞으로 댕김
+                            left= right;
+                            
+                            right = temp;
+                        }
+                    
+                    len = Math.max(len, templen+2);
+                }
+        }
+    return len>2? len:0;
+    
+};
