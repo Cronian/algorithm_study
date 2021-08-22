c9fa7f9fb1b2d0fefdafb95ff26728feb493c5fd
sunjae-Jeong <57482389+nJae94@users.noreply.github.com>
2021-08-22 18:12:34 +0900

    Create Design HashMap.js

---
+++ sj/73week/Design HashMap.js
@@ -0,0 +1,45 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyHashMap = function() {
+
+        this.data = [];
+    
+};
+
+/**
+ * value will always be non-negative. 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function(key, value) {
+    this.data[key] = value;
+};
+
+/**
+ * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function(key) {
+    let val = this.data[key]
+    return val !== undefined ? val : -1
+};
+
+/**
+ * Removes the mapping of the specified value key if this map contains a mapping for the key 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function(key) {
+    delete this.data[key];
+};
+
+/** 
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
