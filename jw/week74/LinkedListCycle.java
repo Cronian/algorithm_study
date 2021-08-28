1dabc1f73b1c572c197cf1a97e9e586de97cb8b5
Cronian <Cronian89@gmail.com>
2021-08-28 23:30:29 +0900

    Create LinkedListCycle.java

---
+++ jw/week74/LinkedListCycle.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && slow != null) {
+            slow = slow.next;
+            fast = fast.next;
+            if(fast != null) fast = fast.next;
+            else break;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+}
