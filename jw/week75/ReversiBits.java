58593c0a6e6c57f782a87abf689e6600363ca928
Cronian <Cronian89@gmail.com>
2021-09-06 22:55:04 +0900

    Update ReversiBits.java

--- jw/week75/ReversiBits.java
+++ jw/week75/ReversiBits.java
@@ -18,3 +18,17 @@ public class Solution {
         return sum;
     }
 }
+
+
+/*
+public int reverseBits(int n) {
+    int result = 0;
+    for (int i = 0; i < 32; i++) {
+        result += n & 1;
+        n >>>= 1;   // CATCH: must do unsigned shift
+        if (i < 31) // CATCH: for last digit, don't shift!
+            result <<= 1;
+    }
+    return result;
+}
+*/
